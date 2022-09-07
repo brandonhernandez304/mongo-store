b21e0466c5882528afde96c65f2510356dd593e6
Brandon Hernandez <bhernandez30497@gmail.com>
2022-09-06 22:17:27 -0400

    index, seed, and new page work. Create WIP

--- models/productSeed.js
+++ models/productSeed.js
@@ -1,30 +1,23 @@
 // SEED
 module.exports = [{
-    title: 'Cracking the Coding Interview',
-    author: 'Gayle Laakmann McDowell',
+    name: 'Hayabusa T3 Boxing Gloves',
+    description: 'Dual-X Hook and Loop Closure, Splinted Wrist Support, 5 Layer Foam Knuckle Padding',
+    img: "https://m.media-amazon.com/images/I/81xd8PIY+oL._AC_SX425_.jpg",
+    price: 159,
+    qty: 50,
 },
 {
-    title: 'HTML and CSS: Design and Build Websites',
-    author: 'Jon Duckett',
+    name: 'Fairtex  MMA Muay Thai Pads',
+    description: 'Curved design creates a tight fit and better accepts leg strikes',
+    img: "https://images-na.ssl-images-amazon.com/images/W/WEBP_402378-T1/images/I/81fl4IZuonL.__AC_SY300_SX300_QL70_FMwebp_.jpg",
+    price: 159,
+    qty: 50,
 },
 {
-    title: 'JavaScript and JQuery: Interactive Front-End Web Development ',
-    author: 'jon Duckett',
-},
-{
-    title: "You Don't Know JS Yet",
-    author: 'Kyle Simpson',
-},
-{
-    title: 'Design Patterns: Elements of Reusable Object-Oriented Software ',
-    author: 'Erich Gamma',
-},
-{
-    title: 'Frontend Unicorn',
-    author: 'Michał Malewicz, Szymon Adamiak, Albert Pawłowski, and Albert Walicki',
-},
-{
-    title: "Don't Make Me Think",
-    author: 'Steve Krug',
+    name: 'SISU Mouthguard',
+    description: 'The slim, custom fit makes it easy to talk, breathe and drink without removing the guard',
+    img: "https://m.media-amazon.com/images/W/WEBP_402378-T1/images/I/71LIKmY9k+S._AC_SX679_.jpg",
+    price: 25,
+    qty: 50,
 },
 ];
